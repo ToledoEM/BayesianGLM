b6741127da109d1a4db87014ff01683da982410a
Enrique <ToledoEM@users.noreply.github.com>
2017-04-20 15:08:44 +0200

    New
    Scripts for whole data set analysis.
    Due to magnitude, all analysis are in parallel.

---
+++ src/whole_dataset_BayesianGLM.R
@@ -0,0 +1,163 @@
+library(pipeR)
+library(rstan)
+library(foreach)
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+print("Modeling entire dataset")
+
+# Function to extract all genes in dataset using readCEF fucntion 
+get.genes.and.cells <- function(DATASET){
+  temp <- readCEF(DATASET)
+  cells <- unique(temp$Cell_type)
+  genes <- rownames(temp$Readcount)
+  metadata <- list(
+    Cell_type=cells,
+    genes=genes)
+  save(metadata,file="OUTPUT/Genes_cell_types.Rdata") #also save it for median dataframe
+  return(metadata)
+}
+
+
+BayesianGLM <- function(DATASET, parallel=T){
+
+  stanmodel <- "
+
+    data {
+      int<lower=0> N;
+      int<lower=0> K;
+      int Y[N];
+      matrix<lower=0>[N,K] X;
+    }
+
+    parameters{
+      vector<lower=1>[K] beta;
+      real<lower=0.001> r;
+    }
+
+    model {
+        vector[N] mu;
+        vector[N] rv;
+        
+        r ~ cauchy(0, 1);
+        beta ~ pareto(1, 1.5);
+        
+        for (n in 1:N){
+            rv[n] = square(r+1) -1;
+        }
+        
+        mu = X * (beta - 1) + 0.001;
+        Y ~ neg_binomial(mu ./ rv, 1 / rv[1]);
+    }
+  " 
+
+  ################
+  # Loading Data #
+  ################
+
+  CEF <- readCEF(DATASET)
+    READCNT <- CEF$Readcount
+    CELLTYP <- CEF$Cell_type
+    CELLTYP[CELLTYP=="mEpen"] <- "mEpend"
+    CELLLEG <- sort(unique(CELLTYP))
+    MOLECNT <- apply(READCNT, 2, sum)
+
+  ##########################################################
+  # Calculation the sizefactor vector (Cell length vector) #
+  ##########################################################
+
+  FACTORS <- list()
+
+  for(i in 1:length(CELLLEG)){
+    TMP <- apply(READCNT[, which(CELLTYP==CELLLEG[i], )], 2, sum)
+    FACTORS[i] <- mean(TMP)
+  }
+
+  FACTORS <- unlist(FACTORS)
+  names(FACTORS) <- CELLLEG
+  SIZFACT <- MOLECNT/FACTORS[CELLTYP]
+
+  ################################################
+  # Create design matrix X(Cell*Celltype matrix) #
+  ################################################
+
+  X0 <- matrix(0, nrow=length(CELLTYP), ncol=length(CELLLEG))
+
+  for(i in 1:length(CELLLEG)){
+    X0[CELLTYP==CELLLEG[i], i] <- 1
+  }
+
+  ########################
+  # Extracting all genes #
+  ########################
+
+  colnames(X0) <- CELLLEG
+  print("extracting genes")
+
+  TGTGENE <- get.genes.and.cells(DATASET)
+  TGTGENE <- TGTGENE$Cell_type
+
+  ############
+  # Run MCMC #
+  ############
+
+  #Try to recapitulate Figure 2G for Mousedataset
+
+  X <- cbind(Sz=SIZFACT, X0)
+  TGT <- which(!is.na(match(rownames(READCNT), TGTGENE)))
+
+  # Compile Stan model
+
+  sm <- stan_model(model_code = stanmodel)
+
+
+  # Parallel computation for each gene
+
+  if(parallel==T){
+
+    library(doParallel)
+
+    cl <- makeCluster(detectCores())
+
+      registerDoParallel(cl)
+
+      foreach(i=TGT, .packages="rstan") %dopar% {
+
+        GENNAME <- rownames(READCNT)[i]
+        Y <- READCNT[i,]
+
+        DATA <- list(Y=Y, X=X, N=length(Y), K=ncol(X))
+
+        OUT <- sampling(sm, data=DATA, iter=1000, chains=2)
+        BETA <- extract(OUT)$beta
+        colnames(BETA) <- colnames(X)
+        OUTRDS <- paste0("OUTPUT/MAP_",GENNAME,".rds",collapse="")
+        saveRDS(BETA, OUTRDS,compress = "bzip2")
+        
+      }
+
+    stopCluster(cl)
+
+  } else {
+
+  # In case of not working parallelization. specify parallel=F
+    
+    foreach(i=TGT, .packages="rstan") %do% {
+
+          GENNAME <- rownames(READCNT)[i]
+          Y <- READCNT[i,]
+
+          DATA <- list(Y=Y, X=X, N=length(Y), K=ncol(X))
+
+          OUT <- sampling(sm, data=DATA, iter=1000, chains=2)
+          BETA <- extract(OUT)$beta
+          colnames(BETA) <- colnames(X)
+          OUTRDS <- paste0("OUTPUT/MAP_",GENNAME,".rds",collapse="")
+          saveRDS(BETA, OUTRDS,compress = "bzip2")
+          
+    }
+
+  }
+
+}
---
+++ src/whole_dataset_median_DF.R
@@ -0,0 +1,37 @@
+#################################################################
+# Calculate medians from MCMC iterations of already saved files #
+# Perform calculations and reading in parallel                  #
+#################################################################
+
+
+Median.data.frame <- function(OUTPUT.folder=OUTPUT){
+  require(dplyr)
+  require(tibble)
+  require(doParallel)
+  
+  #Read METADATA
+  load(file=paste0(OUTPUT.folder,"Genes_cell_types.Rdata",collapse = "")) 
+  Cell_type <- metadata$Cell_type
+  TGTGENE <- metadata$genes
+  
+  #Iterative median calculation in parallel
+  
+  cl <- makeCluster(detectCores())
+  
+  registerDoParallel(cl)
+  
+  MED <- data.frame(row.names = Cell_type)
+  MEDS <- foreach(i=seq_along(TGTGENE),.packages = "dplyr",.combine = cbind) %dopar% {
+    Gene <- TGTGENE[i] 
+    INFILE <- paste0("OUTPUT/MAP_", Gene, ".rds", collapse="")
+    BETA <- (readRDS(INFILE) - 1)[,Cell_type]
+    TEMP <- apply(BETA,MARGIN = 2,FUN = median)
+    MED  <- data.frame(MED,TEMP) %>% rename_(., .dots = setNames("TEMP", Gene))
+    }
+    
+  stopCluster(cl)
+  
+  MEDS <- MEDS %>% t() %>% as.data.frame() %>% rownames_to_column("Features")
+  return(MEDS)
+}
+
